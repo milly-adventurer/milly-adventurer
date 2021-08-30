e58a706cf9c6389728bea704de23d5b1478c9ce5
Evgeniy Khristenko <e.khristenko@ostrovok.ru>
2021-08-30 19:33:01 +0500

    fix

--- constants/url.ts
+++ constants/url.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://milly-back.test8998.workers.dev'; // https://milly-back.test8998.workers.dev'
+export const BASE_URL = 'https://milly-back.test8998.workers.dev'; // https://milly-back.test8998.workers.dev'
 
 export enum URL {
   TOURS = '/tours',
