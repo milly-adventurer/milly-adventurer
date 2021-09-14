1ac171261cb64ec191adbbb05ee12ba41790c119
Evgeny Khristenko <ekhristenko0@gmail.com>
2021-09-14 13:54:21 +0300

    asdf

--- pages/api/hello.tsx
+++ pages/api/hello.tsx
@@ -4,27 +4,27 @@ import { BASE_URL } from '../../constants/url';
 const fs = require("fs");
 
 // Initializing the cors middleware
-const cors = Cors({
-  methods: ['GET', 'HEAD'],
-})
+// const cors = Cors({
+//   methods: ['GET', 'HEAD'],
+// })
 
 // Helper method to wait for a middleware to execute before continuing
 // And to throw an error when an error happens in a middleware
-function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
-  return new Promise((resolve, reject) => {
-    fn(req, res, (result: any) => {
-      if (result instanceof Error) {
-        return reject(result)
-      }
+// function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: any) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result: any) => {
+//       if (result instanceof Error) {
+//         return reject(result)
+//       }
 
-      return resolve(result)
-    })
-  })
-}
+//       return resolve(result)
+//     })
+//   })
+// }
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   // Run the middleware
-  await runMiddleware(req, res, cors);
+  // await runMiddleware(req, res, cors);
 	if (req.method === 'GET') {
 		const path = req.query.id;
 		console.dir(path);
