2f01eb09e60a02c3c7b1ecd3804651c7d06b8f61
Evgeny Khristenko <ekhristenko0@gmail.com>
2022-04-17 14:23:56 +0300

    fix

--- sections/Hero/index.tsx
+++ sections/Hero/index.tsx
@@ -29,7 +29,7 @@ const Hero = ({
 				style={
 					backgroundImage ? {
        			background: backgroundImage.length === 1
-							? `linear-gradient(rgba(0,0,0,0.1), rgba(0,0,0,0.1)), url(${backgroundImage[0]}) center center`
+							? `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url(${backgroundImage[0]}) center center`
 							: undefined,
         		...style,
         	} : style
